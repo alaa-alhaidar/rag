abd0006dbef7ed4739741f1118025f7194a20566
alaa.alhaidar <alaa.alhaidar@hotmail.ccom>
2025-02-03 12:55:19 +0100

    structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rag
+
+# 
+# pip install python-dotenv
--- com/main.py
+++ com/main.py
@@ -1,4 +1,18 @@
+import os
+from dotenv import load_dotenv
 import openai
+# Load environment variables from .env file
+
+
+
 
 if __name__ == '__main__':
     print('Hello, OpenAI!')
+    load_dotenv()
+    # Access environment variables
+    SECRET_KEY = os.getenv("deepseekAPI")
+    DEBUG = os.getenv("DEBUG")
+    print(SECRET_KEY)
+    
+       
+    
